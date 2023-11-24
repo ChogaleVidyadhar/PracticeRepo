1d2599bb6595a645e18336dd95413b48a2a62ce6
vidyadhar <vidyadharchogale143@gmail.com>
2023-11-24 22:02:06 +0530

    new Project Created

---
+++ Practice/src/com/sum/CountSum.java
@@ -0,0 +1,22 @@
+package com.sum;
+
+public class CountSum {
+	
+	public int countSum(int a, int b) {
+		 int sum= a+b;
+		 System.out.println(sum);
+		
+		return sum;
+	}
+	
+
+	public static void main(String[] args) {
+		
+		CountSum cs= new CountSum();
+		cs.countSum(10, 10);
+		
+
+
+	}
+
+}
